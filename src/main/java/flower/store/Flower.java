8204f180b98de823948713c5b0e10463a56caac9
lizalaveikina <116518356+lizalaveikina@users.noreply.github.com>
2023-10-12 22:26:17 +0300

    Update Flower.java

--- src/main/java/flower/store/Flower.java
+++ src/main/java/flower/store/Flower.java
@@ -1,48 +1,30 @@
 package flower.store;
 
+
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
-
-
+import lombok.NoArgsConstructor;
 
 @Setter
-public class Flower extends Item{
-    @Getter
-    private double sepalLength;
-    private FlowerColor color;
-    @Getter
-    private double price;
-    @Getter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Flower extends Item {
     private FlowerType flowerType;
+    private FlowerColor color;
+    private double price;
+    private double sepalLength;
+
+    public Flower(Flower flower) {
+        this.color = flower.color;
+        this.price = flower.price;
+        this.sepalLength = flower.sepalLength;
+        this.flowerType = flower.flowerType;
+    }
 
     public String getColor() {
         return color.toString();
     }
+
 }
-// import lombok.AllArgsConstructor;
-// import lombok.Getter;
-// import lombok.Setter;
-// import lombok.NoArgsConstructor;
-
-// @Setter
-// @Getter
-// @AllArgsConstructor
-// @NoArgsConstructor
-// public class Flower extends Item {
-//     private FlowerType flowerType;
-//     private FlowerColor color;
-//     private double price;
-//     private double sepalLength;
-
-//     public Flower(Flower flower) {
-//         this.color = flower.color;
-//         this.price = flower.price;
-//         this.sepalLength = flower.sepalLength;
-//         this.flowerType = flower.flowerType;
-//     }
-
-//     public String getColor() {
-//         return color.toString();
-//     }
-
-// }
